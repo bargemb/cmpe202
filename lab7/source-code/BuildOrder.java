8263af93e8e25f479b624855faac6e853f7b2a8f
Mayur Barge <bargemayur05@gmail.com>
2018-10-20 23:05:52 -0700

    Fixing bug while adding bun

--- lab7/source-code/BuildOrder.java
+++ lab7/source-code/BuildOrder.java
@@ -1,60 +1,54 @@
-  
+
 
 public class BuildOrder {
 
     public static Component getOrder()
-    {        Composite order = new Composite( "Order" ) ;
+    {
+        Composite order = new Composite( "Order" ) ;
 
         // 1st
         CustomBurger customBurger1 = new CustomBurger( "Build Your Own Burger" ) ;
-        // 1/2 lb
+
         Burger burger1 = new Burger( "Burger Options" ) ;
         String[] burgerOptions1 = { "Organic Bison*", "1/2lb.", "On A Bun" } ;
         burger1.setOptions( burgerOptions1 ) ;
 
-        // extra cheese +1.00
         Cheese cheese1 = new Cheese( "Cheese Options" ) ;
         String[] cheeseOptions1 = {  "Yellow American","Spicy Jalapeno Jack"} ;
         cheese1.setOptions( cheeseOptions1 ) ;
         cheese1.wrapDecorator( burger1 ) ;
 
-        // premium cheese +1.50
         PremiumCheese premiumCheese1 = new PremiumCheese( "Cheese Options" ) ;
         String[] premiumCheeseOptions1 = { "Danish Blue Cheese" } ;
         premiumCheese1.setOptions( premiumCheeseOptions1 ) ;
         premiumCheese1.wrapDecorator( cheese1 ) ;
 
-        // extra sauce +0.50
         Sauce sauce1 = new Sauce( "Sauce Options" ) ;
-        String[] souceOptions1 = { "Mayonnaise" , "Thai Peanut Sauce"} ;
-        sauce1.setOptions( souceOptions1 ) ;
+        String[] sauceOptions1 = { "Mayonnaise", "Thai Peanut Sauce" } ;
+        sauce1.setOptions( sauceOptions1 ) ;
         sauce1.wrapDecorator( premiumCheese1 ) ;
 
-        // toppings free
         Toppings toppings1 = new Toppings( "Toppings Options" ) ;
         String[] toppingOptions1 = { "Dill Pickle Chips","Black Olives","Spicy Pickles" } ;
         toppings1.setOptions( toppingOptions1 ) ;
         toppings1.wrapDecorator( sauce1 ) ;
 
-        // premium topping +1.50
         Premium premiumToppings1 = new Premium( "Premium Options" ) ;
-        String[] premiumToppingOptions = { "Marinated Tomatoes" } ;
-        premiumToppings1.setOptions( premiumToppingOptions ) ;
+        String[] premiumToppingOptions1 = { "Marinated Tomatoes" } ;
+        premiumToppings1.setOptions( premiumToppingOptions1 ) ;
         premiumToppings1.wrapDecorator( toppings1 ) ;
 
-        // Bun
         Bun bun1= new Bun( "Bun Options" ) ;
         String[] bunOptions1 = { "Ciabatta(Vegan)" } ;
         bun1.setOptions( bunOptions1 ) ;
         bun1.wrapDecorator( premiumToppings1) ;
 
-        // extra side +3.00
         Side side1= new Side( "Side Options" ) ;
         String[] sideOptions1 = { "Shoestring Fries" } ;
         side1.setOptions( sideOptions1 ) ;
-        side1.wrapDecorator( burger1) ;
+        side1.wrapDecorator( bun1) ;
 
-        // Setup Custom Burger 1 Ingredients
+        // Setup Custom Burger Ingredients
         customBurger1.setDecorator( side1 ) ;
         customBurger1.addChild( burger1 ) ;
         customBurger1.addChild( cheese1 ) ;
@@ -67,7 +61,7 @@ public class BuildOrder {
 
         // 2nd
         CustomBurger customBurger2 = new CustomBurger( "Build Your Own Burger" ) ;
-        // 1/3 lb
+
         Burger burger2 = new Burger( "Burger Options" ) ;
         String[] burgerOptions2 = { "Hormone & Antibiotic Free Beef*", "1/3lb.", "On A Bun" } ;
         burger2.setOptions( burgerOptions2 ) ;
@@ -88,8 +82,8 @@ public class BuildOrder {
         sauce2.wrapDecorator( premiumCheese2 ) ;
 
         Toppings toppings2 = new Toppings( "Toppings Options" ) ;
-        String[] toppingOptions2 = { "Crushed Peanuts" } ;
-        toppings2.setOptions( toppingOptions2 ) ;
+        String[] toppingsOptions2 = { "Crushed Peanuts" } ;
+        toppings2.setOptions( toppingsOptions2 ) ;
         toppings2.wrapDecorator( sauce2 ) ;
 
         Premium premiumToppings2 = new Premium( "Premium Options" ) ;
@@ -107,7 +101,6 @@ public class BuildOrder {
         side2.setOptions( sideOptions2 ) ;
         side2.wrapDecorator( bun2) ;
 
-        // Setup Custom Burger 2 Ingredients
         customBurger2.setDecorator( side2 ) ;
         customBurger2.addChild( burger2 ) ;
         customBurger2.addChild( cheese2 ) ;
